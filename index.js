b6708e6a135da16551880e83a2e7596004a531b1
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-01 21:28:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData (userName,userEmail){
+  const URL = 'http://localhost:3000/users';
+  const options = {
+  headers: {
+       'Accept': 'application/json',
+      'Content-Type': 'application/json'
+  },
+  method: "POST",
+  body:JSON.stringify({name:userName, email:userEmail})
+}
+return fetch(URL,options)
+.then(res => {
+  return res.json();
+})
+.then(json => {
+  let el = document.createElement('div');
+  el.innerHTML = json.id
+  document.body.appendChild(el);
+}).catch(err => document.body.innerHTML = err.message)
+}
+submitData('John','John@domain.com')
